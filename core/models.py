47fe945b431221ebe61e5fc55b6bcafa1e8db344
gouravchahar13 <1bi22cd014@bit-bangalore.edu.in>
2024-07-11 20:40:47 +0530

    Files

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     address=models.CharField(max_length=100,blank=True)
     total_cases=models.IntegerField()
     cases_names=models.CharField(max_length=100)
-
+    address =models.DateField(auto_now_add=True)
     def __str__(self) -> str:
         return self.name
     
